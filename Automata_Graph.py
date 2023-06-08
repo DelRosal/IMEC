f4b8f6c55227327542e1d32153cb6cf0920325e6
DelRosal <99361062+DelRosal@users.noreply.github.com>
2023-06-08 08:22:44 -0600

    Update Automata_Graph.py

--- Automata_Graph.py
+++ Automata_Graph.py
@@ -13,11 +13,17 @@ graph.node("WAITING")
 graph.attr('node', shape='circle')
   
 #EDGES
+
 graph.edge("","WAITING")
+
+
 graph.edge("WAITING","↑", label='TURN 90')
 graph.edge("WAITING","↓", label='TURN 180')
 graph.edge("WAITING","←", label='TURN 270')
 graph.edge("WAITING","→", label='TURN 360')
+
+graph.edge("WAITING","MOVE", label='MOVE X')
+graph.edge("MOVE","WAITING", label='  ε  ')
 
 graph.edge("↑","PIT STOP", label='  ε  ')
 graph.edge("↓","PIT STOP", label='  ε  ')
@@ -25,9 +31,6 @@ graph.edge("←","PIT STOP", label='  ε  ')
 graph.edge("→","PIT STOP", label='  ε  ')
 
 graph.edge("PIT STOP","WAITING", label='ε')
-
-graph.edge("WAITING","WAITING", label='MOVE X')
-
 
 
 
